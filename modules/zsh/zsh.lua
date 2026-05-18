754dfd6c5f52a0a8a72abd3f4fc76844985bca83
kingdomkind <kingdomkind@protonmail.com>
2026-05-18 13:13:17 +0100

    Update zsh

--- modules/zsh/zsh.lua
+++ modules/zsh/zsh.lua
@@ -2,6 +2,10 @@ local pl = require("pl.path")
 
 return function(lib)
     local body = [[
+
+#> Shows username if root, else, only path
+PS1='[%(!.%n .)%1~]$ '
+
 # Aliases
 alias rm='rm -I'
 alias grep='grep --color=auto'
@@ -51,7 +55,20 @@ ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=009
 ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=009
 ZSH_HIGHLIGHT_STYLES[assign]=none
 
+#> Higlighting
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+#> Vi mode
 source /usr/share/zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+
+#> Auto suggestions
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+#> Fuzzy finder
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
+
+#> Init Zoxide
 export _ZO_ECHO=1
 eval "$(zoxide init zsh --cmd j)"
 export EDITOR=nvim
