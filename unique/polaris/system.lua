466dd1f8c2fc1fd1993670fa2b7b4eb11bada71e
kingdomkind <kingdomkind@protonmail.com>
2026-07-28 06:49:21 +0000

    update laptop config

---
+++ unique/polaris/system.lua
@@ -0,0 +1,78 @@
+return function(lib)
+    local result = lib.imports({}, {
+        lib.modulesd .. "fastfetch/fastfetch.lua",
+        lib.modulesd .. "kitty/kitty.lua",
+        lib.modulesd .. "neovim/neovim.lua",
+        lib.modulesd .. "zsh/zsh.lua",
+        lib.modulesd .. "noctalia/noctalia.lua",
+        lib.modulesd .. "yazi/yazi.lua",
+        { lib.modulesd .. "hypr/hypr.lua", { extension = lib.cwd() .. "hyprext.lua" } },
+    })
+
+    lib.merge(result, {
+        depac = {
+            packages = {
+                --> Core
+                "gcc",
+                "git",
+                "grub",
+                "lua54",
+                "make",
+                "networkmanager",
+                "rustup",
+                "sudo",
+                "intel-ucode",
+                "arch-install-scripts",
+                "artix-archlinux-support",
+                "base",
+                "base-devel",
+                "cryptsetup",
+                "dinit",
+                "efibootmgr",
+                "elogind-dinit",
+                "linux",
+                "linux-headers",
+                "linux-firmware",
+
+                --> Corey
+                "bluez-dinit",
+                "chrony-dinit",
+                "networkmanager-dinit",
+                "openssh",
+                "pipewire-dinit",
+                "pipewire-pulse-dinit",
+                "wireplumber-dinit",
+                "dnsmasq",
+
+                --> Fonts
+                "noto-fonts",
+                "noto-fonts-emoji",
+                "ttf-cascadia-code-nerd",
+
+                "btop",
+                "fastfetch",
+                "less",
+                "unzip",
+                "cosmic-files",
+                "kitty",
+                "firefox",
+                "mpv",
+                "obs-studio",
+                "signal-desktop",
+                "telegram-desktop",
+                "yt-dlp",
+            },
+
+            ignore = {
+                "brave-origin-nightly-bin",
+                "discord-chat-exporter-cli-bin",
+                "grimblast-git",
+                "noctalia-git",
+                "userspawn-git",
+                "yay",
+            }
+        }
+    })
+
+    return result
+end
